b43b1855857c5616adb48b4d0b59c5f1d4d6388b
swatimakhija44 <swati.makhija.ext@bayer.com>
2024-12-27 09:59:24 +0000

    removed unneccsary code

--- elevatesample/src/app/app.routes.ts
+++ elevatesample/src/app/app.routes.ts
@@ -13,9 +13,7 @@ export const routes: Routes = [
           remoteName: 'my-angular-app',
           exposedModule: './routes',
         }).then((r:any) => {
-          console.log("Remote module loaded:", r);
-          console.log("Routes from remote module:", r.routes);
-          return r.routes;  // Make sure to return the routes
+          return r.routes; 
         })
         .catch((error:any) => {
           console.error("Error loading remote module:", error);
