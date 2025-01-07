e2131840cabf2cf67a952ec7a940cd65b70ee8a3
swatimakhija44 <swati.makhija.ext@bayer.com>
2025-01-07 07:42:14 +0000

    iframe code

---
+++ elevatesample/src/app/iframe/iframe.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-iframe',
+  standalone: true,
+  imports: [],
+  templateUrl: './iframe.component.html',
+  styleUrl: './iframe.component.css'
+})
+export class IframeComponent {
+
+}
