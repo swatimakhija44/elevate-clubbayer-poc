1cd904e00d32c319951de47110a1b2bb7b7ff414
swatimakhija44 <swati.makhija.ext@bayer.com>
2025-02-04 05:19:57 +0000

    news shell updated

--- elevate-local-shell/src/app/app.component.spec.ts
+++ elevate-local-shell/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'elevatesample' title`, () => {
+  it(`should have the 'elevate-local-shell' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('elevatesample');
+    expect(app.title).toEqual('elevate-local-shell');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, elevatesample');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, elevate-local-shell');
   });
 });
