5b230ee0a72717d3f2af43796045c239262dcd57
rincy-thomas1 <135584771+rincy-thomas1@users.noreply.github.com>
2025-02-07 05:06:00 +0000

    footer changes

--- clubbayer-poc-angular-app/src/app/app.component.ts
+++ clubbayer-poc-angular-app/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { NewsComponent } from './news/news.component';
 import { MenuComponent } from './menu/menu.component';
 import { HeaderComponent } from './header/header.component'
 import { ClubbayerfooterComponent } from './clubbayerfooter/clubbayerfooter.component'
+import { ClubbayerheaderComponent } from './clubbayerheader/clubbayerheader.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NewsComponent, MenuComponent, HeaderComponent, ClubbayerfooterComponent],
+  imports: [RouterOutlet, NewsComponent, MenuComponent, HeaderComponent, ClubbayerfooterComponent, ClubbayerheaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- clubbayer-poc-angular-app/src/app/clubbayerfooter/clubbayerfooter.component.ts
+++ clubbayer-poc-angular-app/src/app/clubbayerfooter/clubbayerfooter.component.ts
@@ -1,12 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FooterService } from '../service/footer.service';
 
 @Component({
   selector: 'app-clubbayerfooter',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './clubbayerfooter.component.html',
   styleUrl: './clubbayerfooter.component.css'
 })
-export class ClubbayerfooterComponent {
+export class ClubbayerfooterComponent implements OnInit {
 
+  footerMenuItem: any[] = [];
+  footerSubmenuItem: any[] = [];
+  source: any[] = [];
+  loading = true;
+  images: string[] = [];
+  error: string | null = null;
+
+  constructor(private footerService: FooterService) { }
+
+  ngOnInit() {
+    this.fetchFooterMenu();
+  }
+
+  // Updated fetchArticles method using the new fetchCachedToken
+  fetchFooterMenu(): void {
+
+    this.footerService.fetchToken().subscribe(
+      (token) => {
+        if (token) {
+          console.log("aaaaaaaaaaaaaaaa1");
+
+          // Now we get the news using the cached or new token
+          this.footerService.getFooterOne().subscribe(
+            (footerOneData: any[]) => {
+              this.footerMenuItem = footerOneData?.map((item: { title: string; below?: { title: string }[] }) => ({
+                title: item.title,
+                belowTitles: item.below?.map(subItem => subItem.title) ?? []
+              })) ?? [];
+              this.loading = false;
+              // this.fetchImages(this.articles, token);
+              console.log("aaaaaaaaaaaaaaaa", footerOneData);
+              console.log("aaaaaaaaaaaaaaaa1111111", footerOneData[0].title);
+              console.log("aaaaaaaaabbbbb", this.footerMenuItem);
+            },
+            (error) => {
+              console.error('Error fetching news:', error);
+              this.loading = false;
+              this.error = 'Failed to load news.';
+            }
+          );
+        } else {
+          this.loading = false;
+          this.error = 'Failed to retrieve a valid token.';
+        }
+      },
+      (error) => {
+        console.error('Error fetching token:', error);
+        this.loading = false;
+        this.error = 'Failed to retrieve a valid token.';
+      }
+    );
+  }
 }
+
